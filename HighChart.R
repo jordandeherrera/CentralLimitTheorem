13d43ec4d981054d2f37bbbe253799fcab120df7
jsdeherrera <jordan.deherrera@gmail.com>
2015-11-22 15:33:28 -0600

    Created statistical analysis for supplement types and dosages.

--- HighChart.R
+++ HighChart.R
@@ -1,9 +1,32 @@
 library(ggthemes)
 library(ggplot2)
 
-dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
+# Create a boxplot of supplement types and doses
+plot.supplementDose <- (ggplot(aes(x = supp, y = len), data = ToothGrowth) 
+       + geom_boxplot(aes(fill=supp))
+       + facet_wrap(~ dose)
++ theme_few() 
++ scale_colour_few()
++ ggtitle("Tooth Length by Supplement and Dose"))
 
-hc <- (qplot(carat, price, data=dsamp, colour=cut, size=carat)
-+ theme_hc() 
-+ scale_colour_tableau("colorblind10")
-+ ggtitle("Diamonds Are Forever"))
+# Create a boxplot of supplement types
+plot.supplement <- (ggplot(aes(x = supp, y = len), data = ToothGrowth) 
+                        + geom_boxplot(aes(fill=supp))
+                        + theme_few() 
+                        + scale_colour_few()
+                        + ggtitle("Tooth Length by Supplement"))
+
+# Create a boxplot of doses
+plot.dose <- (ggplot(aes(x = dose, y = len), data = ToothGrowth) 
+                    + geom_boxplot(aes(fill=dose))
+                    + facet_grid(. ~ dose)                    
+                    + theme_few() 
+                    + scale_colour_few()
+                    + ggtitle("Tooth Length by Dosage"))
+
+# Create list of plots
+par(cex=0.25)
+plot.list <- list(plot.supplementDose, plot.supplement, plot.dose)
+
+# Grid of all plots
+do.call(grid.arrange, c(plot.list, list(ncol=2)))
